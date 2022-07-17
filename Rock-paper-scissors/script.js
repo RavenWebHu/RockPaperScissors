3204dea940c6a3744a5fcc267be09abb55c57f05
Szilágyi Dániel <szilagyi.dani9520@gmail.com>
2022-07-17 20:05:19 +0200

    Added round outcome, and comp selection marker

--- Rock-paper-scissors/script.js
+++ Rock-paper-scissors/script.js
@@ -1,60 +1,77 @@
-let wins = 0;
-let loses = 0;
-let rounds = 0;
+let wins = 0;                                               //Global variable for counting player wins
+let loses = 0;                                              //Global variable for counting player loses
 
-function computerPlay(){
-    let rn;
+function computerPlay(){                                    // Function for sleceting rock, paper, scissors randomly,
+    let rn;                                                 // and channging the border style for the coresponding img tag
     rn = Math.floor(Math.random() * 9 );
+    let active;
+    let play;
 
     if (rn < 3){
+        play = "comprock";
+        active = document.getElementById(play);
+        active.setAttribute('class', 'compimgactive');
         return "rock";
     }
     else if (rn > 2 && rn < 6){
+        play = "comppaper";
+        active = document.getElementById(play);
+        active.setAttribute('class', 'compimgactive');
         return "paper";
     }
     else{
+        play = "compscissors";
+        active = document.getElementById(play);
+        active.setAttribute('class', 'compimgactive');
         return "scissors";
     }
 }
 
-function playRound(playerSelection, computerSelection)  {
+function playRound(playerSelection, computerSelection)  {   //Function for selecting the winner and returning the round outcome massage 
     let player = playerSelection.toLowerCase();
     let comp = computerSelection;
 
     if (player === "scissors" && comp === "paper"){
-        return "You win, scissors beats paper!"
+        return "You win, scissors beats paper!";
     }
     else if (player === "paper" && comp === "rock"){
-        return "You win, paper beats rock!"
+        return "You win, paper beats rock!";
     }
     else if (player === "rock" && comp === "scissors"){
-        return "You win, rock beats scissors!"
+        return "You win, rock beats scissors!";
     }
     else if (comp === "scissors" && player === "paper"){
-        return "You lose, scissors beats paper!"
+        return "You lose, scissors beats paper!";
     }
     else if (comp === "paper" && player === "rock"){
-        return "You lose, paper beats rock!"
+        return "You lose, paper beats rock!";
     }
     else if (comp === "rock" && player === "scissors"){
-        return "You lose, rock beats scissors!"
+        return "You lose, rock beats scissors!";
     }
     else if (comp === "rock" && player === "rock"){
-        return "It's a tie!"
+        return "It's a tie!";
     }
     else if (comp === "paper" && player === "paper"){
-        return "It's a tie!"
+        return "It's a tie!";
     }
     else if (comp === "scissors" && player === "scissors"){
-        return "It's a tie!"
-    }
-    else {
-        return "Spock is not a valid option here :)";
+        return "It's a tie!";
     }
  
 }
+function resetSelection(){
+    let div = document.getElementById('compselector');
+    let compimgs = div.querySelectorAll('img');
+    compimgs.forEach(resetCSS);
+}
+function resetCSS(img){
+    img.setAttribute('class', 'compimg');
+}
+
 
 function game(selection){
+    resetSelection();
     let computerSelection = computerPlay();
     let playerSelection = selection;
 
@@ -62,30 +79,23 @@ function game(selection){
         
         if(round.includes("win")){
             wins++;
-            rounds++;
         }
         else if(round.includes("tie")){
             return;
         }
         else{
             loses++;
-            rounds++;
         }
 
         document.getElementById('playerw').innerHTML = wins;
         document.getElementById('compw').innerHTML = loses;
+        document.getElementById('roundstat').innerHTML = round;
 
-        if (rounds === 5){
+        if (wins + loses === 5){
             alert("Round is over");
             wins = 0;
             loses = 0;
-            rounds = 0;
         }
-
-
-
-        console.log(computerSelection);
-        console.log(round);
-    
+  
 
 }
